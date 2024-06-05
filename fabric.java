52024698743f88ff89e72b2c333e59f8ab480633
Max Rydahl Andersen <max@xam.dk>
2024-06-05 14:57:35 +0200

    basic help

--- fabric.java
+++ fabric.java
@@ -12,48 +12,46 @@
 //FILES application.properties
 
 import java.net.URI;
-import java.util.List;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.net.http.*;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.model.StreamingResponseHandler;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
-import dev.langchain4j.model.output.Response;
-import io.quarkiverse.langchain4j.openai.OpenAiRestApi;
-import io.quarkus.logging.Log;
-import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
-import io.smallrye.config.ConfigMapping;
-import jakarta.enterprise.context.Dependent;
-import jakarta.enterprise.context.control.ActivateRequestContext;
-import jakarta.inject.Inject;
-import picocli.CommandLine;
-import picocli.CommandLine.Option;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-@CommandLine.Command
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.output.Response;
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+@CommandLine.Command(description = """
+        An open source framework for augmenting humans using AI.
+
+        Pick a pattern from https://github.com/danielmiessler/fabric/blob/main/patterns/ to use.
+            """)
 public class fabric implements Runnable {
 
-@Option(names = {"-p", "--pattern"}, description = "The pattern (prompt) to use", required = true)
-String pattern;
+    @Option(names = { "-p", "--pattern" }, description = "The pattern (prompt) to use", required = true)
+    String pattern;
 
-@Option(names = {"-t", "--text"}, description = "The text to use")
-String text;
+    @Option(names = { "-t", "--text" }, description = "The text to use")
+    String text;
 
-@Option(names = {"-s", "--stream"}, description = "Stream the response")
-boolean stream;
-
-
+    @Option(names = { "-s", "--stream" }, description = "Stream the response")
+    boolean stream;
 
     private final AI ai;
 
@@ -73,9 +71,8 @@ boolean stream;
             }
         }
 
-        
-        String result =  ai.prompt(pattern, text, stream);
-        if(!stream) {
+        String result = ai.prompt(pattern, text, stream);
+        if (!stream) {
             System.out.println(result);
         }
     }
@@ -92,30 +89,35 @@ class AI {
     UserMessage user(String text) {
         return UserMessage.from(text);
     }
-    @Inject ChatLanguageModel clm;
+
+    @Inject
+    ChatLanguageModel clm;
 
     String prompt(String pattern, String text, boolean stream) {
 
-
-        String urlPattern = "https://raw.githubusercontent.com/danielmiessler/fabric/main/patterns/%s/".formatted(pattern);
+        String urlPattern = "https://raw.githubusercontent.com/danielmiessler/fabric/main/patterns/%s/"
+                .formatted(pattern);
 
         var user = downloadContent(urlPattern + "user.md");
         var system = downloadContent(urlPattern + "system.md");
 
         List<ChatMessage> messages = new ArrayList<>();
-        if(system!=null && !system.isBlank()) messages.add(system(system));
-        if(user!=null && !user.isBlank()) messages.add(user(user));
-        if(text!=null && !text.isBlank()) messages.add(user(text));
+        if (system != null && !system.isBlank())
+            messages.add(system(system));
+        if (user != null && !user.isBlank())
+            messages.add(user(user));
+        if (text != null && !text.isBlank())
+            messages.add(user(text));
 
         Log.info("Asking AI");
-        if(stream) {
+        if (stream) {
             CompletableFuture<String> future = new CompletableFuture<>();
 
             sclm.generate(messages, new StreamingResponseHandler<AiMessage>() {
-              
+
                 @Override
                 public void onNext(String token) {
-                   System.out.print(token);
+                    System.out.print(token);
                 }
 
                 @Override
@@ -140,18 +142,17 @@ class AI {
 
     }
 
-    @Inject StreamingChatLanguageModel sclm;
+    @Inject
+    StreamingChatLanguageModel sclm;
 
-
-
-    //todo: vertx or other utility method implementation
+    // todo: vertx or other utility method implementation
     private String downloadContent(String url) {
         String def = null;
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
+                    .uri(URI.create(url))
+                    .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             return response.statusCode() == 200 ? response.body() : def;
         } catch (Exception e) {
@@ -159,5 +160,5 @@ class AI {
             return def;
         }
     }
-    
+
 }
