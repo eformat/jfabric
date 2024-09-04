41d15c0595f151af9d2a4fe597f22b02ccb8ccd5
Mike Hepburn <eformat@gmail.com>
2024-09-04 14:33:18 +1000

    🎯 escape document filename 🎯

--- app.py
+++ app.py
@@ -123,7 +123,6 @@ if st.session_state.start_chat:
             for f in uploaded_files:
                 file_path = os.path.join(DOCUMENT_SOURCE_DIRECTORY, f.name)
                 cmd = "java -jar tika-app-3.0.0-BETA2.jar --text '" + file_path + "' | jbang run fabric.java -p create_app3 -s"
-                #print(os.popen(cmd).read())
                 print(f"[INFO] Processing {f.name}")
                 my_env = os.environ.copy()
                 my_env["INFERENCE_SERVER_URL"] = service_url
